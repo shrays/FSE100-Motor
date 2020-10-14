38006e21845331eb5baff52d1799dd5df978b019
ShraySwarup <shrayswarup@gmail.com>
2020-10-13 22:41:01 -0700

    Base Code: Working Buttons

---
+++ fms-game/single_sketch.js
@@ -0,0 +1,20 @@
+
+function preload() {
+  
+}
+
+function setup() {
+  createCanvas(1280,720);
+}
+
+function draw() {
+  noStroke();
+  noFill("");
+  fill("orange");
+  rect1 = rect(484,423,323,97);//matching(1)
+}
+
+function TracingGame(){
+
+}
+
