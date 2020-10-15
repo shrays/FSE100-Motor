be30ac053543769e53f934d1522efed0dbbfc81d
ShraySwarup <shrayswarup@gmail.com>
2020-10-15 15:40:52 -0700

    Button 1 File Adds

---
+++ js/game_1.js
@@ -0,0 +1,19 @@
+
+function preload() {
+  
+}
+
+function setup() {
+  createCanvas(1280,720);
+}
+
+function draw() {
+  noStroke();
+  noFill("");
+  fill("green");
+  rect1 = rect(484,423,323,97);//matching(1)
+}
+
+function TracingGame(){
+
+}
