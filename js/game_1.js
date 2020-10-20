da78c7b623244da82a3df0a315d6652a8003ce24
Shray Swarup <shrayswarup@gmail.com>
2020-10-20 14:24:39 -0700

    Moving Squares JS 1
    Working movement and selection indicators

--- js/game_1.js
+++ js/game_1.js
@@ -1,100 +1,168 @@
-
-let rectangle,circle;
-var img;
-
-function preload(){
-img = loadImage('Images/spaceBackground.png');
-}
-
+let shape1;
+let shape2;
+let shape3;
+let shape4;
+let shape5;
 
 function setup() {
-
-  noStroke();
-  fill(0,0,0,0);
-  createCanvas(1280, 720);
-
-  let x = 150, y = 150, y1 = 200, w = 125, h = 100;
-
-  rectangle = new Rectangle(x, y, w, h);
-
- // fill("orange");
-  circle = new Circle(x,y1,w,h);
-
-
+  createCanvas(640, 360);
+  shape1 = new Draggable(40, 80, 50, 50);
+  shape2 = new Draggable(150, 80, 50, 50);
+  shape3 = new Draggable(40, 160, 50, 50);
+  shape4 = new Draggable(150, 160, 50, 50);
+  shape5 = new Draggable(100, 250, 50, 50);
 }
 
 function draw() {
-  clear();
-  rectangle.show(mouseX, mouseY);
-  circle.show(mouseX, mouseY);
+  background(255);
+  shape1.over();
+  shape1.update();
+  shape1.show();
+  shape2.over();
+  shape2.update();
+  shape2.show();
+  shape3.over();
+  shape3.update();
+  shape3.show();
+  shape4.over();
+  shape4.update();
+  shape4.show();
+  shape5.over();
+  shape5.update();
+  shape5.show();
+
 }
 
-
 function mousePressed() {
-  rectangle.pressed(mouseX, mouseY);
-  circle.pressed(mouseX, mouseY);
+  shape1.pressed();
+  shape2.pressed();
+  shape3.pressed();
+  shape4.pressed();
+  shape5.pressed();
 }
 
 function mouseReleased() {
-  rectangle.notPressed();
-  circle.notPressed();
-}
-
-class Circle {
-  constructor(x, y1, w, h) {
-    this.x = x;
-    this.y1 = y1;
-    this.w = w;
-    this.h = h;
-    this.offsetX = 0;
-    this.offsetY = 0;
-    this.dragging = false;
-    this.rollover = false;
-  }
+  shape1.released();
+  shape2.released();
+  shape3.released();
+  shape4.released();
+  shape5.released();
   
 }
 
-
-class Rectangle {
+class Draggable {
   constructor(x, y, w, h) {
+    this.dragging = false; // Is the object being dragged?
+    this.rollover = false; // Is the mouse over the ellipse?
     this.x = x;
     this.y = y;
     this.w = w;
     this.h = h;
     this.offsetX = 0;
     this.offsetY = 0;
-    this.dragging = false;
-    this.rollover = false;
+  }
+  
+  
+
+  over() {
+    // Is mouse over object
+    if (mouseX > this.x && mouseX < this.x + this.w && mouseY > this.y && mouseY < this.y + this.h) {
+      this.rollover = true;
+    } else {
+      this.rollover = false;
+    }
   }
 
-  show(px, py) {
+  update() {
+    // Adjust location if being dragged
     if (this.dragging) {
-      this.x = px + this.offsetX;
-      this.y = py + this.offsetY;
+      this.x = mouseX + this.offsetX;
+      this.y = mouseY + this.offsetY;
     }
+  }
 
-    stroke(255);
-    noFill();
+  show() {
+    stroke(0);
+    // Different fill based on state
+    if (this.dragging) {
+      fill(50);
+    } else if (this.rollover) {
+      fill(100);
+    } else {
+      fill(175, 200);
+    }
     rect(this.x, this.y, this.w, this.h);
   }
 
-  pressed(px, py) {
-    if (px > this.x && px < this.x + this.w && py > this.y && py < this.y + this.h) {
-      //print("clicked on rect");
+  pressed() {
+    // Did I click on the rectangle?
+    if (mouseX > this.x && mouseX < this.x + this.w && mouseY > this.y && mouseY < this.y + this.h) {
       this.dragging = true;
-      this.offsetX = this.x - px;
-      // print(this.offsetX);
-      this.offsetY = this.y - py;
-      // print(this.offsetY);
+      // If so, keep track of relative location of click to corner of rectangle
+      this.offsetX = this.x - mouseX;
+      this.offsetY = this.y - mouseY;
     }
   }
 
-  notPressed(px, py) {
-    	//print("mouse was released");
-      this.dragging = false;
+  released() {
+    // Quit dragging
+    this.dragging = false;
   }
 }
 
+class DraggableS {
+  constructor(x, y, d) {
+    this.dragging = false; // Is the object being dragged?
+    this.rollover = false; // Is the mouse over the ellipse?
+    this.x = x;
+    this.y = y;
+    this.d = d;
+    this.offsetX = 0;
+    this.offsetY = 0;
+  }
 
+  over() {
+    // Is mouse over object
+    if (mouseX > this.x && mouseX < this.x + this.w && mouseY > this.y && mouseY < this.y + this.h) {
+      this.rollover = true;
+    } else {
+      this.rollover = false;
+    }
+  }
 
+  update() {
+    // Adjust location if being dragged
+    if (this.dragging) {
+      this.x = mouseX + this.offsetX;
+      this.y = mouseY + this.offsetY;
+    }
+  }
 
+  show() {
+    stroke(0);
+    // Different fill based on state
+    if (this.dragging) {
+      fill(50);
+    } else if (this.rollover) {
+      fill(100);
+    } else {
+      fill(175, 200);
+    }
+    rect(this.x, this.y, this.w, this.h);
+  }
+
+  pressed() {
+    // Did I click on the rectangle?
+    if (mouseX > this.x && mouseX < this.x + this.w && mouseY > this.y && mouseY < this.y + this.h) {
+      this.dragging = true;
+      // If so, keep track of relative location of click to corner of rectangle
+      this.offsetX = this.x - mouseX;
+      this.offsetY = this.y - mouseY;
+    }
+  }
+
+  released() {
+    // Quit dragging
+    this.dragging = false;
+  }
+}
